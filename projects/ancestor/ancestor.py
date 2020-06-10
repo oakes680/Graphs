77faddc4c95e7f72fe4fed847a7d9cf35c3f3ccf
Chris Oakes <chris.oakes@outlook.com>
2020-06-09 20:21:26 -0700

    day1 and day2 mvp

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,73 +1,73 @@
 from util import Stack, Queue
 
-class Graph:
-    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+# class Graph:
+#     """Represent a graph as a dictionary of vertices mapping labels to edges."""
 
-    def __init__(self):
-        self.vertices = {}
+#     def __init__(self):
+#         self.vertices = {}
 
-    def add_vertex(self, vertex_id):
-        """
-        Add a vertex to the graph.
-        """
-        self.vertices[vertex_id] = set()
+#     def add_vertex(self, vertex_id):
+#         """
+#         Add a vertex to the graph.
+#         """
+#         self.vertices[vertex_id] = set()
 
-    def add_edge(self, v1, v2):
-        """
-        Add a directed edge to the graph.
-        """
-        if v1 in self.vertices and v2 in self.vertices:
-            self.vertices[v1].add(v2)
+#     def add_edge(self, v1, v2):
+#         """
+#         Add a directed edge to the graph.
+#         """
+#         if v1 in self.vertices and v2 in self.vertices:
+#             self.vertices[v1].add(v2)
 
-    def get_neighbors(self, vertex_id):
-        """
-        Get all neighbors (edges) of a vertex.
-        """
-        return self.vertices[vertex_id]
-    def dft(self, starting_vertex):
-        """
-        Print each vertex in depth-first order
-        beginning from starting_vertex.
-        """
-        s = Stack()
-    #   s = [v,neightbor1, neightbor2]
-        # LIFO
-        s.push(starting_vertex)
-        visited = set()
-        while s.size() > 0:
-            v = s.pop()
-            if v not in visited:
-                print(v)
-                visited.add(v)
-                for vertex in self.get_neighbors(v):
-                    s.push(vertex)
+#     def get_neighbors(self, vertex_id):
+#         """
+#         Get all neighbors (edges) of a vertex.
+#         """
+#         return self.vertices[vertex_id]
+#     def dft(self, starting_vertex):
+#         """
+#         Print each vertex in depth-first order
+#         beginning from starting_vertex.
+#         """
+#         s = Stack()
+#     #   s = [v,neightbor1, neightbor2]
+#         # LIFO
+#         s.push(starting_vertex)
+#         visited = set()
+#         while s.size() > 0:
+#             v = s.pop()
+#             if v not in visited:
+#                 print(v)
+#                 visited.add(v)
+#                 for vertex in self.get_neighbors(v):
+#                     s.push(vertex)
 
-    def dfs(self, starting_vertex, destination_vertex):
-            """
-            Return a list containing a path from
-            starting_vertex to destination_vertex in
-            depth-first order.
-            """
-            visited = set()
-            path = [starting_vertex]
-            if starting_vertex not in self.vertices or destination_vertex not in self.vertices:
-                return None
-            if starting_vertex == destination_vertex:
-                return path
-            s = Stack()
-            s.push(path)
+#     def dfs(self, starting_vertex, destination_vertex):
+#             """
+#             Return a list containing a path from
+#             starting_vertex to destination_vertex in
+#             depth-first order.
+#             """
+#             visited = set()
+#             path = [starting_vertex]
+#             if starting_vertex not in self.vertices or destination_vertex not in self.vertices:
+#                 return None
+#             if starting_vertex == destination_vertex:
+#                 return path
+#             s = Stack()
+#             s.push(path)
             
-            while s.size() > 0:
-                path = s.pop()
-                node = path[-1]
-                if node not in visited:
-                    visited.add(node)
-                    for vertex in self.get_neighbors(node):
-                        new_path = list(path)
-                        new_path.append(vertex)
-                        s.push(new_path)
-                        if vertex == destination_vertex:
-                            return new_path
+#             while s.size() > 0:
+#                 path = s.pop()
+#                 node = path[-1]
+#                 if node not in visited:
+#                     visited.add(node)
+#                     for vertex in self.get_neighbors(node):
+#                         new_path = list(path)
+#                         new_path.append(vertex)
+#                         s.push(new_path)
+#                         if vertex == destination_vertex:
+#                             return new_path
         
     
 
@@ -82,6 +82,7 @@ class Graph:
     #     6   7   9
     # '''
     # ancesotrs [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]
+
 def earliest_ancestor(ancestors, starting_node):
     s = Stack()
     s.push(starting_node)
@@ -96,19 +97,25 @@ def earliest_ancestor(ancestors, starting_node):
     while s.size() > 0:
         popped = s.pop()
         parent = popped
-        if popped not in visited:
+        if popped not in visited: 
             visited.add(popped)
-            for popped in get_neighbors1(popped, ancestors):
-                s.push(popped)
+            for neighbor in get_neighbors1(parent, ancestors):
+             
+                s.push(neighbor)
+            
     
     return parent
 
 
 def get_neighbors1(node, ancestors):
-    neighbors = [ancestor[0] for ancestor in ancestors if ancestor[1] == node]
+    # neighbors = [ancestor[0] for ancestor in ancestors if ancestor[1] == node]
+    neighbors = []
+    for ancestor in ancestors:
+        if ancestor[1] == node:
+            neighbors.append(ancestor[0])
     return neighbors
 
 
+print(earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)], 6))
 
-    
-print(earliest_ancestor([(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)], 9))
+# print(get_neighbors1(4, [(1, 3), (2, 3), (3, 6), (5, 6), (5, 7), (4, 5), (4, 8), (8, 9), (11, 8), (10, 1)]))
